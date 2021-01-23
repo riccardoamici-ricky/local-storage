5468e9e6f1287d3fe42a5e808245aecccb368d3b
Riccardo <riccardo@baako.de>
2021-01-23 17:54:57 +0100

    local storage update

--- myscript.js
+++ myscript.js
@@ -2,7 +2,19 @@ const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = JSON.parse(localStorage.getItem('items'))|| [];
 
+function addItem(e) {
+  e.preventDefault(); // preventing the loading of the page
+  const text = (this.querySelector('[name=item]')).value;
+  const item = {
+    text,
+    done: false
+  };
 
+  items.push(item);
+  populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
+  this.reset();
+}
 
 function populateList(plates = [], platesList) {
   platesList.innerHTML = plates.map((plate, i) => {
