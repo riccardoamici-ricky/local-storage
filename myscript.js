b6fda2816edb409e5348b83546a63bb2d1371051
Riccardo <riccardo@baako.de>
2021-01-23 16:21:52 +0100

    created function populateList

--- myscript.js
+++ myscript.js
@@ -11,6 +11,18 @@ function addItem(e) {
   };
 
   items.push(item);
+  populateList(items, itemsList);
   this.reset();
 }
+
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates.map((plate, i) => {
+    return `
+      <li>
+        <label for="">${plate.text}</label>
+      </li>
+    `;
+  }).join('');
+}
+
 addItems.addEventListener('submit', addItem);
